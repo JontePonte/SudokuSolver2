31f4741d11c81fffc7d5a94e8cfc76dc906bd059
JontePonte <john_nordstrand@hotmail.com>
2020-06-28 22:48:04 +0200

    added find singel possibilities for columns and boxes

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ from create_rows_columns_boxes import create_rows, create_columns, create_boxes
 
 # Solving functions
 from simple_remove_possibilities import simple_remove_possibilities_row, simple_remove_possibilities_column, simple_remove_possibilities_boxe
+from find_single_possibilities import find_single_possibilities_row, find_single_possibilities_column, find_single_possibilities_box
 
 
 # Create field objects for the sudoku
@@ -19,18 +20,6 @@ columns = create_columns(fields)
 boxes = create_boxes(fields)
 
 
-def find_single_possibilities_row(field, row):
-    for num in field.possible:
-        num_found_times = 0
-        for other_field in row:
-            if other_field.id != field.id:
-                if num in other_field.possible:
-                    num_found_times += 1
-        if num_found_times == 0:
-            return {num}
-    return field.possible
-
-
 for i in range(10):
     for field in fields:
         field.possible = simple_remove_possibilities_row(field, rows[field.y])
@@ -38,6 +27,8 @@ for i in range(10):
         field.possible = simple_remove_possibilities_boxe(field, boxes[field.box])
 
         field.possible = find_single_possibilities_row(field, rows[field.y])
+        field.possible = find_single_possibilities_column(field, columns[field.x])
+        field.possible = find_single_possibilities_box(field, boxes[field.box])
 
         field.number = check_field_possible_number(field)
 
---
+++ find_single_possibilities.py
@@ -0,0 +1,50 @@
+
+def find_single_possibilities_row(field, row):
+    """ Check if a possibility in the field only appears ones in a row. Sets the possibility to that value if so """
+    # Only check the unset numbers
+    if field.number == 0:
+        for num in field.possible:
+            # The num_found_times is the amount of times the possibility has been found in other fields
+            num_found_times = 0
+            for other_field in row:
+                if other_field.id != field.id:
+                    if num in other_field.possible:
+                        num_found_times += 1
+            # If the possibility only is found in the working field then that is our number!
+            if num_found_times == 0:
+                return {num}
+    return field.possible
+
+
+def find_single_possibilities_column(field, column):
+    """ Check if a possibility in the field only appears ones in a column. Sets the possibility to that value if so """
+    # Only check the unset numbers
+    if field.number == 0:
+        for num in field.possible:
+            # The num_found_times is the amount of times the possibility has been found in other fields
+            num_found_times = 0
+            for other_field in column:
+                if other_field.id != field.id:
+                    if num in other_field.possible:
+                        num_found_times += 1
+            # If the possibility only is found in the working field then that is our number!
+            if num_found_times == 0:
+                return {num}
+    return field.possible
+
+
+def find_single_possibilities_box(field, box):
+    """ Check if a possibility in the field only appears ones in a box. Sets the possibility to that value if so """
+    # Only check the unset numbers
+    if field.number == 0:
+        for num in field.possible:
+            # The num_found_times is the amount of times the possibility has been found in other fields
+            num_found_times = 0
+            for other_field in box:
+                if other_field.id != field.id:
+                    if num in other_field.possible:
+                        num_found_times += 1
+            # If the possibility only is found in the working field then that is our number!
+            if num_found_times == 0:
+                return {num}
+    return field.possible
