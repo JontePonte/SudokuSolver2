b69485ba222c18c54dcbaecebb481798f643c353
JontePonte <john_nordstrand@hotmail.com>
2020-06-30 22:41:00 +0200

    the new remove extra poss if two survives the unit test now

--- remove_poss_two_numbers.py
+++ remove_poss_two_numbers.py
@@ -38,19 +38,27 @@ def remove_extra_poss_if_two(list_input):
                 # this is every test set
                 test_set = {num1, num2}
 
+                num1_counter = 0
+                num2_counter = 0
                 ts_found_counter = 0
                 ts_id = []
-                # Run throu all fields and check if their possibilities are the same as the test set
+                # Run throu all fields and check if the test set appears in them
+                # also check if the numbers appears by the selves
                 for field in list_work:
-                    if test_set == field.possible:
-                        ts_found_counter += 1           # Count how many times they appear
-                        ts_id.append(field.id)          # and store the field id
-                
-                # If the test set appears two times in the fields remove the test set from every other field
-                if ts_found_counter == 2:
-                    for field in list_work:
-                        if not field.id in ts_id:
-                            field.possible.discard(list(test_set)[0])
-                            field.possible.discard(list(test_set)[1])
+                    if {num1}.issubset(field.possible) and {num2}.issubset(field.possible):
+                        ts_found_counter += 1
+                        ts_id.append(field.x)
+
+                    if {num1}.issubset(field.possible):
+                        num1_counter += 1
+
+                    if {num2}.issubset(field.possible):
+                        num2_counter += 1
+
+                # If the test set appears two times in the fields and in no other field
+                # then remove all other possibilities in the fields
+                if ts_found_counter == 2 and num1_counter == 2 and num2_counter == 2:
+                    list_work[ts_id[0]].possible = test_set
+                    list_work[ts_id[1]].possible = test_set
     
     return list_work
