b07357c3bcd1dbe557aef9768d508205e877ab41
JontePonte <john_nordstrand@hotmail.com>
2020-06-30 22:04:37 +0200

    prepared function and test to remove extra possibilities if two poss just exists in two fields

--- test_remove_poss_set_two.py
+++ test_remove_poss_set_two.py
@@ -66,4 +66,34 @@ class TestRemovePossSetTwo(unittest.TestCase):
         self.assertEqual(row_1_result[5].possible, {5})        
         self.assertEqual(row_1_result[6].possible, {3})        
         self.assertEqual(row_1_result[7].possible, {4})        
+        self.assertEqual(row_1_result[8].possible, {4,5,7,9})
+
+
+def test_remove_extra_poss_if_two_row(self):
+        row_1 = self.rows[1]    # The second row has a "0" in the first field
+        # second row: [0, 0, 6, 8, 0, 5, 3, 0, 0]
+        
+        # All possible needs to be set manualy for this test
+        row_1[0].possible = {1,2}
+        row_1[1].possible = {2,4,5,7,9}
+        row_1[2].possible = {6}
+        row_1[3].possible = {8}
+        row_1[4].possible = {1,2}
+        row_1[5].possible = {5}
+        row_1[6].possible = {3}
+        row_1[7].possible = {1,2,4}
+        row_1[8].possible = {4,5,7,9}
+
+        row_1_result = rem_poss.remove_extra_poss_if_two(row_1)
+
+        # The 1 and 2 should be removed from every field exept 0 and 4 becasue 1 and 2 have to be in 0 and 4,
+        # all other possibilities should stay the same
+        self.assertEqual(row_1_result[0].possible, {1,2})
+        self.assertEqual(row_1_result[1].possible, {4,5,7,9})        
+        self.assertEqual(row_1_result[2].possible, {6})        
+        self.assertEqual(row_1_result[3].possible, {8})        
+        self.assertEqual(row_1_result[4].possible, {1,2})        
+        self.assertEqual(row_1_result[5].possible, {5})        
+        self.assertEqual(row_1_result[6].possible, {3})        
+        self.assertEqual(row_1_result[7].possible, {4})        
         self.assertEqual(row_1_result[8].possible, {4,5,7,9})        
