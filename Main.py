c24fa252654b4eb882ee8c5eaad337863dfaf8b3
JontePonte <john_nordstrand@hotmail.com>
2020-07-15 18:35:06 +0200

    Created function from code in Main

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 """ Main file for SudokuSolver2"""
 
-from Sudokus import sudoku          # The sudoku is chosen in the Sudokus file
+from Sudokus import sudoku as sudoku_import          # The sudoku is chosen in the Sudokus file
 from FieldClass import Field
 from auxiliary_functions import (load_sudoku, 
                                  check_field_possible_number, 
@@ -21,92 +21,74 @@ from remove_poss_three_numbers import (remove_poss_just_three,
                                        remove_extra_poss3_field)
 from recursive_solution import RecursivSolv
 
+def solve_sudoku(sudoku):
+    # Create field objects for the sudoku
+    fields = load_sudoku(sudoku)
+    fields_old = load_sudoku(sudoku)
 
-# Create field objects for the sudoku
-fields = load_sudoku(sudoku)
-fields_old = load_sudoku(sudoku)
-
-# Create lists for all rows, column and boxes
-rows = create_rows(fields)
-columns = create_columns(fields)
-boxes = create_boxes(fields)
-
-
-# Loop all the fields max 20 times and run all solving functions. 
-counter = 1
-while not is_solved(fields) and counter < 20:
-    for field in fields:
-        # Remove possibilities if field if they appears in the same row, colomn or box
-        field.possible = simple_remove_possibilities(field, rows[field.y])
-        field.possible = simple_remove_possibilities(field, columns[field.x])
-        field.possible = simple_remove_possibilities(field, boxes[field.box])
-
-        # Remove all other possibilities if a possibility only appears ones in a row, column or box
-        field.possible = find_single_possibilities(field, rows[field.y])
-        field.possible = find_single_possibilities(field, columns[field.x])
-        field.possible = find_single_possibilities(field, boxes[field.box])
-        
-        # If a pair of two possibilities just appears in two fields in a row, column or box then remove them from all other
-        # them from all other fields in that row, column or box
-        for row in rows:
-            row = remove_poss_just_two(row)
-        for column in columns:
-            column = remove_poss_just_two(column)
-        for box in boxes:
-            box = remove_poss_just_two(box)
-        
-        # Remove extra possibilities in a field if two of the possibilities just appear in one other
-        # field in the row, column or box
-        field.possible = remove_extra_poss_field(field, rows[field.y])
-        field.possible = remove_extra_poss_field(field, columns[field.x])
-        field.possible = remove_extra_poss_field(field, boxes[field.box])
-        
-        """ 
-                The functions testing three solutions are commented out to increase prefomance
-                All sudokus can be solved using the recursive solution instead
-        
-        # If a pair of three possibilities just appears in three fields in a row, column or box then remove them from all other
-        # them from all other fields in that row, column or box
-        for row in rows:
-            row = remove_poss_just_three(row)
-        for column in columns:
-            column = remove_poss_just_three(column)
-        for box in boxes:
-            box = remove_poss_just_three(box)
-        
-        # Remove extra possibilities in a field if three of the possibilities just appear in two other
-        # field in the row, column or box
-        field.possible = remove_extra_poss3_field(field, rows[field.y])
-        field.possible = remove_extra_poss3_field(field, columns[field.x])
-        field.possible = remove_extra_poss3_field(field, boxes[field.box])
-        
-        """
-        
-        # Set the finds number if there is only one possibility
-        field.number = check_field_possible_number(field)
-    counter += 1        
+    # Create lists for all rows, column and boxes
+    rows = create_rows(fields)
+    columns = create_columns(fields)
+    boxes = create_boxes(fields)
 
 
-# If the solution method cant solve the sudoku in 20 iterations the recursive solution kiks in
-if not is_solved(fields):
-    used_recursive = True
-    sudo = RecursivSolv(fields)             # Call recursive solution method
-    fields = load_sudoku(sudo.sudoku_num)   # Update fields from RecursiveSolve output
-else:
-    used_recursive = False                  # Just for print output
+    # Loop all the fields max 20 times and run all solving functions. 
+    counter = 1
+    while not is_solved(fields) and counter < 20:
+        for field in fields:
+            # Remove possibilities if field if they appears in the same row, colomn or box
+            field.possible = simple_remove_possibilities(field, rows[field.y])
+            field.possible = simple_remove_possibilities(field, columns[field.x])
+            field.possible = simple_remove_possibilities(field, boxes[field.box])
+
+            # Remove all other possibilities if a possibility only appears ones in a row, column or box
+            field.possible = find_single_possibilities(field, rows[field.y])
+            field.possible = find_single_possibilities(field, columns[field.x])
+            field.possible = find_single_possibilities(field, boxes[field.box])
+            
+            # If a pair of two possibilities just appears in two fields in a row, column or box then remove them from all other
+            # them from all other fields in that row, column or box
+            for row in rows:
+                row = remove_poss_just_two(row)
+            for column in columns:
+                column = remove_poss_just_two(column)
+            for box in boxes:
+                box = remove_poss_just_two(box)
+            
+            # Remove extra possibilities in a field if two of the possibilities just appear in one other
+            # field in the row, column or box
+            field.possible = remove_extra_poss_field(field, rows[field.y])
+            field.possible = remove_extra_poss_field(field, columns[field.x])
+            field.possible = remove_extra_poss_field(field, boxes[field.box])
+            
+            # Set the finds number if there is only one possibility
+            field.number = check_field_possible_number(field)
+        counter += 1        
 
 
-print(" ")
-if is_solved(fields):
-    if not used_recursive:
-        print("The sudoku was solved after", str(counter), "iterations")
+    # If the solution method cant solve the sudoku in 20 iterations the recursive solution kiks in
+    if not is_solved(fields):
+        used_recursive = True
+        sudo = RecursivSolv(fields)             # Call recursive solution method
+        fields = load_sudoku(sudo.sudoku_num)   # Update fields from RecursiveSolve output
     else:
-        print("The sudoku was solved after", str(counter), "normal iterations")
-        print( "and", str(sudo.counter), "recursive solution interations")
-else:
-    print("The sudoku was not solved after", str(counter), "iterations")
-    print(count_zeros(fields), "unsolved fields")
+        used_recursive = False                  # Just for print output
 
 
-# The original and the hopfully solved sudoku are printed side by side
-print_two_sudokus(fields_old, fields)
+    print(" ")
+    if is_solved(fields):
+        if not used_recursive:
+            print("The sudoku was solved after", str(counter), "iterations")
+        else:
+            print("The sudoku was solved after", str(counter), "normal iterations")
+            print( "and", str(sudo.counter), "recursive solution interations")
+    else:
+        print("The sudoku was not solved after", str(counter), "iterations")
+        print(count_zeros(fields), "unsolved fields")
+
+
+    # The original and the hopfully solved sudoku are printed side by side
+    print_two_sudokus(fields_old, fields)
+
+
+solve_sudoku(sudoku_import)
